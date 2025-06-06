d5fd667974f16e3a51490686c77c800ba6546025
CMCFame <jetronico@gmail.com>
2025-06-06 15:19:13 -0600

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+// Este componente es la plantilla principal de toda tu aplicación.
+// La línea de arriba importa los estilos globales para que se apliquen en todas las páginas.
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
